b42294e6030ad6419c75e08deb0a466fc1dcc518
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2021-07-25 15:22:09 +0100

    Extends app and updates entry point

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose");
+const router = require('./routes/task/task');
+const userRoutes = require('./routes/user');
+const cors = require('cors')
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("Connected to the database"))
+
+app.use("/task", router)
+app.use('/user', userRoutes)
+
+app.listen(process.env.PORT, ()=>{
+   console.log(`server running on port ${process.env.PORT}`)
+})
