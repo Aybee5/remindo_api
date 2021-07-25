2b8c9f5004ab741e98b2ce3a950889370f55782d
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2021-07-25 15:21:42 +0100

    Extends

--- jobs/agenda.js
+++ jobs/agenda.js
@@ -1,8 +1,6 @@
 const Agenda =  require('agenda')
+require('dotenv').config();
 const agenda = new Agenda({db: {address: process.env.DB_URL, collection: 'agenda'}})
-
 agenda.on("ready", async ()=> await agenda.start())
-
-agenda.define("Daily Email ")
 
 module.exports = agenda
